a4579121c2d90f0abbfe012825b8b5e3e9ea4749
koitt03-A <koitt03-A@koitt03-A-PC>
2018-08-22 14:28:56 +0900

    54

--- ict_erp_khg/src/com/ict/erp/dao/LevelDAO.java
+++ ict_erp_khg/src/com/ict/erp/dao/LevelDAO.java
@@ -6,5 +6,11 @@ import java.util.List;
 import com.ict.erp.vo.LevelInfo;
 
 public interface LevelDAO {
-	public List<LevelInfo> selectLiList(LevelInfo li)throws SQLException ;
+	public List<LevelInfo> selectLiList(LevelInfo li) throws SQLException;
+
+	public int insertLevel(List<LevelInfo> liList) throws SQLException;
+
+	public int deleteLiList(int[] liNums) throws SQLException;
+
+	public int updateLiList(List<LevelInfo> liList) throws SQLException;
 }
