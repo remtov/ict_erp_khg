c78d8733245b0b092660fdd5d6c9689d3f17d8dc
koitt03-A <koitt03-A@koitt03-A-PC>
2018-08-22 15:09:45 +0900

    fd

--- ict_erp_khg/src/com/ict/erp/service/LevelService.java
+++ ict_erp_khg/src/com/ict/erp/service/LevelService.java
@@ -2,9 +2,13 @@ package com.ict.erp.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import com.ict.erp.vo.LevelInfo;
 
 public interface LevelService {
-	public List<LevelInfo> getLiList(LevelInfo li)throws SQLException ;
+	public List<LevelInfo> getLiList(LevelInfo li) throws SQLException ;
+	public Map<String,Object> insertNupdateLiList(Map<String,List<LevelInfo>>liList) throws SQLException;
+	public Map<String,Object> deleteLiList(List<LevelInfo>liList)throws SQLException;
+
 }
