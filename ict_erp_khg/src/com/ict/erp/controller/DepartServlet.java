0fb6928fc794c9361364dc153e1ec82d5c30442f
koitt03-A <koitt03-A@koitt03-A-PC>
2018-08-23 14:41:17 +0900

    hghj

--- ict_erp_khg/src/com/ict/erp/controller/DepartServlet.java
+++ ict_erp_khg/src/com/ict/erp/controller/DepartServlet.java
@@ -1,27 +1,32 @@
 package com.ict.erp.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet(
-		urlPatterns = "/depart/*", 
-		name = "DepartServlet"	
-)
+@WebServlet(urlPatterns = "/depart/*", name = "DepartServlet",loadOnStartup=1)
 
 public class DepartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		doService(req, res);
 
 	}
 
-protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doService(req, res);
 	}
 
+	public void doService(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String uri = "/views" + req.getRequestURI();
+		RequestDispatcher rd = req.getRequestDispatcher(uri);
+		rd.forward(req, res);
+	}
 }
