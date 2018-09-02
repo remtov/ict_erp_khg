d002913569a5b871d0b2107165175f38872929d4
dd <dd@dd-PC>
2018-09-03 00:43:48 +0900

    453543

--- ict_erp_khg/src/com/ict/erp/service/TicketService.java
+++ ict_erp_khg/src/com/ict/erp/service/TicketService.java
@@ -7,10 +7,10 @@ import java.util.List;
 import com.ict.erp.vo.TicketMovie;
 
 public interface TicketService {
-
+	public List<TicketMovie> selectTmList(TicketMovie tm) throws SQLException;
 	public TicketMovie selectTm(TicketMovie tm) throws SQLException;
 	public int insertTm(TicketMovie tm) throws SQLException;
 	public int updateTm(TicketMovie tm) throws SQLException;
 	public int deleteTm(TicketMovie tm) throws SQLException;
-	public List<TicketMovie> selectTmList(TicketMovie tm) throws SQLException;
+
 }
